e30e0dfdb736a9b28cf11784e04a3a53cd9ef818
Nadya Shakila Lopulisa <nnad0019@student.monash.edu>
2026-04-26 05:54:48 +0800

    Fix extensions file

--- extensions.py
+++ extensions.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-=======
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
->>>>>>> 04d5b17500d4a2f875841ad4991309f74d25b740
